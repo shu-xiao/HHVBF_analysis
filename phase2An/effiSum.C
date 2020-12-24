8d8c05aff41e73fb3ffe9a955f3ec5de93a2768f
Shu-Xiao Liu <d3a2s1l@gmail.com>
2020-12-24 19:27:28 +0800

    update plots

--- phase2An/effiSum.C
+++ phase2An/effiSum.C
@@ -13,15 +13,18 @@ void effiSum() {
     vector <int> nEve_VBF_HHTo4B_CV_1_C2V_2_C3_1   = {499656  ,222699  ,222699   ,149427  ,108360 ,33632  ,25446  ,9237  ,906  ,906};
     vector <int> nEve_VBF_HHTo4B_CV_1_C2V_1_C3_1   = {499652  ,13300   ,13300    ,4339    ,1934   ,468    ,285    ,131   ,11   ,11};
     vector <int> nEve_VBF_HHTo4B_CV_1_C2V_1_C3_0   = {500000  ,10208   ,10208    ,2800    ,1465   ,445    ,306    ,123   ,10   ,10};
+    vector <int> nEve_VBF_HHTo4B_CV_1_C2V_1_C3_2   = {499308  ,26649   ,26649    ,9038    ,2068   ,509    ,204    ,94    ,11   ,11};
     vector <int> nEve_QCD_600toInf                 = {50000   ,48487   ,48487    ,45994   ,8659   ,307    ,0};
     vector <int> nEve_TT                           = {300000  ,10270   ,2964     ,1778    ,239    ,22     ,4      ,0};
     
     TH1F * h1 = new TH1F("h1","",10,0,10);
     TH1F * h2 = new TH1F("h2","",10,0,10);
     THStack *hs_effi = new THStack("hffi","cut efficiency X 100");
+    THStack *hssig_effi = new THStack("hffi_sig","PU200 signal cut efficiency X 100");
     THStack *hs_cumeffi = new THStack("hffi","cumulative cut efficiency");
+    THStack *hs_sigeffi = new THStack("hffi_sig","PU200 signal cumulative cut efficiency");
     vector<string> namelist = {"QCD_600toInf","TT","VBF_HHTo4B_CV_1_5_C2V_1_C3_1","VBF_HHTo4B_CV_0_5_C2V_1_C3_1","VBF_HHTo4B_CV_1_C2V_2_C3_1",\
-    "VBF_HHTo4B_CV_1_C2V_1_C3_1","VBF_HHTo4B_CV_1_C2V_1_C3_0"};
+    "VBF_HHTo4B_CV_1_C2V_1_C3_1","VBF_HHTo4B_CV_1_C2V_1_C3_0","VBF_HHTo4B_CV_1_5_C2V_1_C3_2"};
 
     TH1F* heffi_QCD_600toInf = new TH1F("h_QCD_600toInf","",10,0,10);
     TH1F* heffi_TT = new TH1F("h_TT","",10,0,10);
@@ -30,6 +33,7 @@ void effiSum() {
     TH1F* heffi_VBF_HHTo4B_CV_1_C2V_2_C3_1 = new TH1F("h_VBF_HHTo4B_CV_1_C2V_2_C3_1","",10,0,10);
     TH1F* heffi_VBF_HHTo4B_CV_1_C2V_1_C3_1 = new TH1F("h_VBF_HHTo4B_CV_1_C2V_1_C3_1","",10,0,10);
     TH1F* heffi_VBF_HHTo4B_CV_1_C2V_1_C3_0 = new TH1F("h_VBF_HHTo4B_CV_1_C2V_1_C3_0","",10,0,10);
+    TH1F* heffi_VBF_HHTo4B_CV_1_C2V_1_C3_2 = new TH1F("h_VBF_HHTo4B_CV_1_C2V_1_C3_2","",10,0,10);
 
     TH1F* hcumeffi_QCD_600toInf = new TH1F("hcum_QCD_600toInf","",10,0,10);
     TH1F* hcumeffi_TT = new TH1F("hcum_TT","",10,0,10);
@@ -38,21 +42,22 @@ void effiSum() {
     TH1F* hcumeffi_VBF_HHTo4B_CV_1_C2V_2_C3_1 = new TH1F("hcum_VBF_HHTo4B_CV_1_C2V_2_C3_1","",10,0,10);
     TH1F* hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_1 = new TH1F("hcum_VBF_HHTo4B_CV_1_C2V_1_C3_1","",10,0,10);
     TH1F* hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_0 = new TH1F("hcum_VBF_HHTo4B_CV_1_C2V_1_C3_0","",10,0,10);
+    TH1F* hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_2 = new TH1F("hcum_VBF_HHTo4B_CV_1_C2V_1_C3_1","",10,0,10);
 
     vector<vector<int>> dataList = {nEve_QCD_600toInf, nEve_TT,\
         nEve_VBF_HHTo4B_CV_1_C2V_2_C3_1,nEve_VBF_HHTo4B_CV_0_5_C2V_1_C3_1,\
         nEve_VBF_HHTo4B_CV_1_5_C2V_1_C3_1,nEve_VBF_HHTo4B_CV_1_C2V_1_C3_1,\
-        nEve_VBF_HHTo4B_CV_1_C2V_1_C3_0};
+        nEve_VBF_HHTo4B_CV_1_C2V_1_C3_0,nEve_VBF_HHTo4B_CV_1_C2V_1_C3_2};
 
     vector<TH1F*> hlist = {heffi_QCD_600toInf, heffi_TT, \
         heffi_VBF_HHTo4B_CV_1_C2V_2_C3_1, heffi_VBF_HHTo4B_CV_0_5_C2V_1_C3_1, \
         heffi_VBF_HHTo4B_CV_1_5_C2V_1_C3_1,heffi_VBF_HHTo4B_CV_1_C2V_1_C3_1,\
-        heffi_VBF_HHTo4B_CV_1_C2V_1_C3_0};
+        heffi_VBF_HHTo4B_CV_1_C2V_1_C3_0,heffi_VBF_HHTo4B_CV_1_C2V_1_C3_2};
 
     vector<TH1F*> hcumlist = {hcumeffi_QCD_600toInf, hcumeffi_TT, \
         hcumeffi_VBF_HHTo4B_CV_1_C2V_2_C3_1, hcumeffi_VBF_HHTo4B_CV_0_5_C2V_1_C3_1, \
         hcumeffi_VBF_HHTo4B_CV_1_5_C2V_1_C3_1,hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_1,\
-        hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_0};
+        hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_0,hcumeffi_VBF_HHTo4B_CV_1_C2V_1_C3_2};
     
     vector<string> cutList = {"nTotal","nJet","eta","pt","tau21","SDMass","DeepAk8","AK4","AK4eta","invMass"};
     for (int i=0;i<cutList.size();i++) {
@@ -65,7 +70,8 @@ void effiSum() {
     for (int j=0;j<cutList.size();j++) h2->Fill(cutList[j].data(),0); 
     
     c1->Print("test.pdf[");
-    int colorList[] = {kBlack,kGray+1,kRed,kOrange,kPink+10,kViolet,kMagenta};
+    int colorList[] = {kBlack,kGray+1,kRed,kOrange,kPink-4,kViolet,kMagenta-9,kAzure};
+    int markerList[] = {kFullCircle,kFullSquare,kFullTriangleUp,kFullStar,kFullDiamond,kFullCross};
     for (int i=0;i<hlist.size();i++) {
         h1->Reset();
         h2->Reset();
@@ -85,9 +91,11 @@ void effiSum() {
         hcumlist[i]->Scale(1./dataList[i][0]);
         hcumlist[i]->SetLineColor(colorList[i]);
         hcumlist[i]->SetLineWidth(2);
-        hs_cumeffi->Add(hcumlist[i],"L");
+        hs_cumeffi->Add(hcumlist[i],"");
+        if (i>1) hs_sigeffi->Add(hcumlist[i],"");
     }
-    
+    TH1F* h_empty = (TH1F*)hlist[0]->Clone("");
+    h_empty->Reset();
     auto mg = new TMultiGraph();
     mg->SetTitle("cumulative cut efficiency");
     vector<TGraph*> tgList;
@@ -102,16 +110,35 @@ void effiSum() {
     for (int i=0;i<tgList.size();i++) mg->Add(tgList[i],"APL");
     for (int i=0;i<hlist.size();i++) {
         hlist[i]->SetFillColor(colorList[i]);
+        hlist[i]->SetLineColor(colorList[i]);
+        hlist[i]->SetLineWidth(0);
         hlist[i]->Scale(100);
         hs_effi->Add(hlist[i],"hist text 0");
+        if (i>1) {
+            hlist[i]->SetMarkerStyle(markerList[i-2]);
+            hlist[i]->SetMarkerSize(1);
+    }
+        if(i>1) hssig_effi->Add(hlist[i],"hist text 0");
     }
     hs_effi->Draw("nostackb");
     gPad->BuildLegend(0.85,0.75,0.99,0.95,"","F");
+    c1->Print("test.pdf");
+    //hssig_effi->GetYaxis()->SetTitle("Efficiency (%)");
+    hssig_effi->Draw("nostackb");
+    auto leg = gPad->BuildLegend(0.85,0.75,0.99,0.95,"","F");
+    hssig_effi->Add(h_empty,"hist text 0");
+    hssig_effi->Draw("nostackb");
+    leg->Draw();
     c1->Print("test.pdf");
 
 
     //gStyle->SetHistLineWidth(2);
     hs_cumeffi->Draw("nostack");
+    gPad->BuildLegend(0.8,0.75,0.99,0.95,"","");
+    c1->SetLogy();
+    c1->Print("test.pdf");
+    //hs_sigeffi->GetYaxis()->SetTitle("Cumulative Efficiency");
+    hs_sigeffi->Draw("nostack");
     gPad->BuildLegend(0.8,0.75,0.99,0.95,"","");
     c1->SetLogy();
     c1->Print("test.pdf");
